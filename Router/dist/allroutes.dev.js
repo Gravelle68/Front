6e218569a7064feb8a8187fd0acb92f107e7d5ee
Gravelle68 <gravelle68@hotmail.com>
2024-06-06 18:11:07 +0200

    page reserver

--- Router/dist/allroutes.dev.js
+++ Router/dist/allroutes.dev.js
@@ -10,7 +10,7 @@ var _Route = _interopRequireDefault(require("./Route.js"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
 //Définir ici vos routes
-var allRoutes = [new _Route["default"]("/", "Accueil", "/pages/home.html"), new _Route["default"]("/galerie", "La galerie", "/pages/galerie.html"), new _Route["default"]("/signin", "connexion", "/pages/auth/signin.html"), new _Route["default"]("/signup", "Inscription", "/pages/auth/signup.html"), new _Route["default"]("/account", "Mon compte", "/pages/auth/account.html"), new _Route["default"]("/editPassword", "Changement de mot de passe", "/pages/auth/editPassword.html"), new _Route["default"]("/allResa", "Vos réservations", "/pages/reservations/allResa.html")]; //Le titre s'affiche comme ceci : Route.titre - websitename
+var allRoutes = [new _Route["default"]("/", "Accueil", "/pages/home.html"), new _Route["default"]("/galerie", "La galerie", "/pages/galerie.html"), new _Route["default"]("/signin", "connexion", "/pages/auth/signin.html"), new _Route["default"]("/signup", "Inscription", "/pages/auth/signup.html"), new _Route["default"]("/account", "Mon compte", "/pages/auth/account.html"), new _Route["default"]("/editPassword", "Changement de mot de passe", "/pages/auth/editPassword.html"), new _Route["default"]("/allResa", "Vos réservations", "/pages/reservations/allResa.html"), new _Route["default"]("/reserver", "Réserver", "/pages/reservations/reserver.html")]; //Le titre s'affiche comme ceci : Route.titre - websitename
 
 exports.allRoutes = allRoutes;
 var websiteName = "Quai Antique";
